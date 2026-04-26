2c8e709458a1bc15cb1abbfda5184530b7594c26
juliana <eilynjb08@gmail.com>
2026-04-26 13:49:27 -0500

    Primer commit: Estructura inicial de Nutrik

---
+++ src/componentes/Botones.jsx
@@ -0,0 +1,11 @@
+function Botones() {
+  return (
+    <nav>
+      <button>Inicio</button>
+      <button>Comprar</button>
+      <button>Iniciar Sesión</button>
+    </nav>
+  );
+}
+
+export default Botones;

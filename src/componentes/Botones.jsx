5efdb55be27e4a0f1cdf4fa3bac915e087e7475a
juliana <eilynjb08@gmail.com>
2026-04-26 22:45:56 -0500

    Conectando botones del menú principal con las rutas

--- src/componentes/Botones.jsx
+++ src/componentes/Botones.jsx
@@ -1,10 +1,23 @@
+import { Link } from 'react-router-dom';
+
 function Botones() {
   return (
-    <nav>
-      <button>Inicio</button>
-      <button>Comprar</button>
-      <button>Iniciar Sesión</button>
-    </nav>
+    <div className="botones-container">
+      {/* Botón de Inicio: lleva a la raíz / */}
+      <Link to="/">
+        <button className="btn-nav">Inicio</button>
+      </Link>
+
+      {/* Botón de Registrarse: lleva a tu página /registro */}
+      <Link to="/registro">
+        <button className="btn-nav">Registrarse</button>
+      </Link>
+
+      {/* Botón de Iniciar Sesión: lleva a tu página /login */}
+      <Link to="/login">
+        <button className="btn-nav">Iniciar Sesión</button>
+      </Link>
+    </div>
   );
 }
 
